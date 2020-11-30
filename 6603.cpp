54f136c2ad4e733ebf2a8012a2f747fd303fab59
march-dave <ysangy@gmail.com>
2020-11-30 18:13:27 -0500

    6603

--- 6603.cpp
+++ 6603.cpp
@@ -1,16 +1,16 @@
 // https://www.acmicpc.net/problem/6603
 // select 6 numbers from the set, Carefully review Edge case
-// K = 7, S = [1 2 3 4 5 6 7], 
+// K = 7, S = [1 2 3 4 5 6 7]
 
 #include <iostream>
 using namespace std;
 
-int K, S;
-int arr[20];
+int K;
+int S[13], arr[20];
 // int ans;
 
 void DFS(int level, int sum) {
-    if(level == S) {
+    if(level == S.size()) {
         // if (sum == S) {
         //     ans = ans + 1;
         // }
@@ -22,18 +22,18 @@ void DFS(int level, int sum) {
 
 int main() {
 
-    cin >> K >> S;
+    cin >> K;
     for (int i=0; i<K; i++) {
-        cin >> arr[i];
+        cin >> S[i];
     }
 
     DFS(0, 0);
     // cout << "kk" << (S == 0);
     // cout << ans - (S == 0);
 
-    for(int j=0; j<S; j++) {
-        cout << arr[j];
-    }
+    // for(int j=0; j<S.size(); j++) {
+    //     cout << arr[j];
+    // }
     // if (S == 0) {
     //     cout << ans - 1;
     // } else {
