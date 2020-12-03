4be93394a3d36bd91ae28c0b975076c79370f223
march-dave <ysangy@gmail.com>
2020-12-02 19:34:41 -0500

    6603

--- 6603.cpp
+++ 6603.cpp
@@ -9,16 +9,16 @@ int K;
 int S[13], arr[20];
 // int ans;
 
-void DFS(int level, int sum) {
-    if(level == S.size()) {
-        // if (sum == S) {
-        //     ans = ans + 1;
-        // }
-    } else {
-        DFS(level+1, sum);
-        DFS(level+1, sum+arr[level]);    
-    }
-}
+// void DFS(int level, int sum) {
+//     if(level == S.size()) {
+//         // if (sum == S) {
+//         //     ans = ans + 1;
+//         // }
+//     } else {
+//         DFS(level+1, sum);
+//         DFS(level+1, sum+arr[level]);    
+//     }
+// }
 
 int main() {
 
@@ -27,13 +27,13 @@ int main() {
         cin >> S[i];
     }
 
-    DFS(0, 0);
+    // DFS(0, 0);
     // cout << "kk" << (S == 0);
     // cout << ans - (S == 0);
 
-    // for(int j=0; j<S.size(); j++) {
-    //     cout << arr[j];
-    // }
+    for(int j=0; j<S.size(); j++) {
+        cout << arr[j];
+    }
     // if (S == 0) {
     //     cout << ans - 1;
     // } else {
