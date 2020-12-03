8bc4a1fa6c7728e11047bc96e284bf3df323de08
march-dave <ysangy@gmail.com>
2020-12-02 20:18:33 -0500

    6603

--- 6603.cpp
+++ 6603.cpp
@@ -7,37 +7,34 @@ using namespace std;
 
 int K;
 int S[13], arr[20];
+int combi[200];
 // int ans;
 
-// void DFS(int level, int sum) {
-//     if(level == S.size()) {
-//         // if (sum == S) {
-//         //     ans = ans + 1;
-//         // }
-//     } else {
-//         DFS(level+1, sum);
-//         DFS(level+1, sum+arr[level]);    
-//     }
-// }
+void DFS(int level, int sum) {
+    if(level == 6) {
+        // if (sum == S) {
+        //     ans = ans + 1;
+        // }
+    } else {
+        for(int i=sum; i<K; i++) {
+            combi[level] = arr[i];
+            DFS(level+1, sum+arr[level]);
+        }
+    }
+}
 
 int main() {
 
-    cin >> K;
-    for (int i=0; i<K; i++) {
-        cin >> S[i];
+    while(cin >> K && K) {
+        for (int i=0; i<K; i++) {
+            cin >> S[i];
+        }
     }
 
-    // DFS(0, 0);
-    // cout << "kk" << (S == 0);
-    // cout << ans - (S == 0);
+    DFS(0, 0);
 
-    for(int j=0; j<S.size(); j++) {
-        cout << arr[j];
+    for(int j=0; j<6; j++) {
+        cout << combi[j];
     }
-    // if (S == 0) {
-    //     cout << ans - 1;
-    // } else {
-    //     cout << ans;
-    // }
     return 0;
 }
