192e491929ae180a8625023500939be61286f7d1
march-dave <ysangy@gmail.com>
2020-12-07 08:16:26 -0500

    vowel and consonant apply DFS level from global value

--- 1759.cpp
+++ 1759.cpp
@@ -12,35 +12,35 @@
 #include <algorithm> // sort
 using namespace std;
 
-int L, C; // L =4 , C = 6
+int L, C; // L = 4 , C = 6
 char arr[20];
 char passW[4];
-int vowel, consonant;
+// int vowel, consonant;
 
-void DFS(int level, int num) {
+void DFS(int level, int num, int vowel, int consonant) {
     if (level == L) {
         if ( vowel >= 1 && consonant >= 2) {
-            for(int i=0; i<4; i++) {
-                cout << passW[i] << ' ';
+            for(int i=0; i<L; i++) {
+                cout << passW[i];
             }
+            // cout << " vowel: " << vowel;
+            // cout << " consonant: " << consonant;
+            // cout << "\n";
         }
     } else {
         for(int i=num; i<C; i++) {
             if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') {
                 passW[level] = arr[i];
-                vowel = vowel + 1;
-                DFS(level+1, i+1);
+                DFS(level+1, i+1, vowel+1, consonant);
             } else {
                 passW[level] = arr[i];
-                consonant = consonant + 1;
-                DFS(level+1, i+1);
+                DFS(level+1, i+1, vowel, consonant+1);
             }
         }
     }
 }
 
 int main() {
-
     cin >> L;
     cin >> C;
 
@@ -49,10 +49,7 @@ int main() {
     }
 
     sort(arr, arr+C);
-
-    DFS(0, 0);
+    DFS(0, 0, 0, 0);
 
     return 0;
 }
-
-
