de73dfe0a2b843464bcdc5af208a62b69df59d66
march-dave <ysangy@gmail.com>
2020-12-06 19:21:54 -0500

    checking vowel and consonant

--- 1759.cpp
+++ 1759.cpp
@@ -12,19 +12,29 @@
 #include <algorithm> // sort
 using namespace std;
 
-int L, C;
+int L, C; // L =4 , C = 6
 char arr[20];
 char passW[4];
+int vowel, consonant;
 
 void DFS(int level, int num) {
-    if (level == 4) {
-        for(int i=0; i<4; i++) {
-            cout << passW[i];
+    if (level == L) {
+        if ( vowel >= 1 && consonant >= 2) {
+            for(int i=0; i<4; i++) {
+                cout << passW[i];
+            }
         }
     } else {
         for(int i=num; i<L; i++) {
-            passW[level] = arr[i];
-            DFS(level+1, i+1);
+            if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') {
+                passW[level] = arr[i];
+                vowel = vowel + 1;
+                DFS(level+1, i+1);
+            } else {
+                passW[level] = arr[i];
+                consonant = consonant + 1;
+                DFS(level+1, i+1);
+            }
         }
     }
 }
