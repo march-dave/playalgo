73edba902a57b1274e5f75d5a1b8e50d2a06afe5
march-dave <ysangy@gmail.com>
2020-12-07 19:35:26 -0500

    2661

---
+++ 2661.cpp
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/2661
+// 숫자 1, 2, 3으로만 이루어지는 수열이 있다
+// 입력은 숫자 N하나로 이루어진다. N은 1 이상 80 이하이다.
+// 2
+// 32
+// 32123
+// 1232123 finally we are excepted this number.
+
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
