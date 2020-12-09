dbfa66a3819e2b2b6a489e1fc374b7a60d6fbd31
march-dave <ysangy@gmail.com>
2020-12-08 19:02:07 -0500

    2661 continue

--- 2661.cpp
+++ 2661.cpp
@@ -1,14 +1,29 @@
 // https://www.acmicpc.net/problem/2661
 // 숫자 1, 2, 3으로만 이루어지는 수열이 있다
 // 입력은 숫자 N하나로 이루어진다. N은 1 이상 80 이하이다.
-// 2
-// 32
-// 32123
-// 1213121 finally we are excepted this number.
 
 #include <iostream>
+using namespace std;
+
+int N; // 1<= N <= 80 scope
+// N = 7
+// 1213121 finally we are expected this number.
+
+void DFS(int level, int num) {
+    if(level == N) {
+
+    } else {
+
+        for(int i=N; i<N; i++) {
+            DFS(level+1, i+1);
+        }
+    }
+
+}
 
 int main() {
+    cin >> N;
+    DFS(0, 0);
 
     return 0;
 }
