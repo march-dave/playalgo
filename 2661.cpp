2d194fe01b7da42bc6d54cdef847813edad4c349
march-dave <ysangy@gmail.com>
2020-12-10 19:51:01 -0500

    2661 continue

--- 2661.cpp
+++ 2661.cpp
@@ -27,3 +27,41 @@ int main() {
 
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int arr[88];
+// int n;
+
+// bool valid(int idx){
+// 	int cnt = 0;
+// 	for(int i=2; i<=idx/2; i++){ //dist
+// 		for(int j=1; j<=idx-i; j++){ //start
+// 			cnt = 0;
+// 			for(int k=j; k<i+j; k++){
+// 				if(i+k > idx) continue;
+// 				if(arr[k] == arr[i+k]) cnt++;
+// 			}
+// 			if(cnt == i) return 0;
+// 		}
+// 	}
+// 	return 1;
+// }
+
+// void f(int idx){
+// 	if(idx > n){
+// 		for(int i=1; i<=n; i++) cout << arr[i];
+// 		exit(0);
+// 	}
+// 	for(int i=1; i<=3; i++){
+// 		if(arr[idx-1] == i) continue;
+// 		arr[idx] = i;
+// 		if(valid(idx)) f(idx+1);
+// 	}
+// }
+
+// int main(){
+// 	cin >> n;
+// 	f(1);
+// }
