bfa8a5597a9313b1baab589793cbc5124398320a
march-dave <ysangy@gmail.com>
2020-12-14 00:09:24 -0500

    2661 continue

--- 2661.cpp
+++ 2661.cpp
@@ -10,24 +10,42 @@ int arr[80];
 // N = 7
 
 // 1213121 finally we are expected this number.
+bool valid(int level) {
+    int cnt = 0;
+    for(int i=2; i<=level/2; i++) {
+        for(int j=1; j<=level-1; j++) {
+            cnt = 0;
+            for(int k=j; k<i+j; k++) {
+                if(i+k > level) continue;
+                if(arr[k] == arr[i+k])
+                cnt++;
+            }
+            if(cnt==i) return 0;
+        }
+    }
+
+    return 1;
+}
 
 void DFS(int level) {
-    if(level == N) {
-
+    if(level-1 == N) {
+        for(int i=0; i<N; i++) {
+            cout << arr[i];
+        }
+        exit(0);
     } else {
 
         for(int i=1; i<3; i++) {
-
             if(arr[level-1] == i) continue;            
             arr[level] = i;
-            DFS(level+1);
+            if (valid(level)) DFS(level+1);
         }
     }
 }
 
 int main() {
     cin >> N;
-    DFS(0);
+    DFS(1);
 
     return 0;
 }
