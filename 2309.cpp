13e0de077e9166d879652fe0d23a8417351aafd1
march-dave <ysangy@gmail.com>
2020-12-15 19:44:07 -0500

    2309

---
+++ 2309.cpp
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/2309
+// 일곱 난쟁이
+// 아홉 개의 줄에 걸쳐 난쟁이들의 키가 주어진다. 주어지는 키는 100을 넘지 않는 자연수이며
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
