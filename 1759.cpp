5920e7d9ad9928d8e9a3ba1b37ab0e2da0471ac9
march-dave <ysangy@gmail.com>
2020-12-29 17:54:59 -0500

    2309

--- 1759.cpp
+++ 1759.cpp
@@ -52,3 +52,37 @@ int main() {
 
     return 0;
 }
+
+
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+  int sum = 0;
+  int i, j, k, n = 9;
+  int arr[n];
+  arr[0] = 0;
+
+  for (i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+    sum += arr[i];
+  }
+
+  sort(arr, arr+n);
+
+  for (i = 0; i < n; i++) {
+    for (j = i+1; j < n; j++) {
+      if (sum - arr[i] - arr[j] == 100) {
+        for (k = 0; k < n; k++) {
+          if (k == i || k == j) continue;
+          printf("%d\n", arr[k]);
+        }
+        return 0;
+      }
+    }
+  }
+
+  return 0;
+}
